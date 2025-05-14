74db5a54f47e17e725f4a3973e9f57bc074786cc
Naomi Lita <naomi.placuta@gmail.com>
2025-05-14 15:12:31 +0300

    fixed header warning and log out btn

--- locafy/src/main/java/com/locafy/locafy/controllers/BusinessOwnerController.java
+++ locafy/src/main/java/com/locafy/locafy/controllers/BusinessOwnerController.java
@@ -1,10 +1,24 @@
 package com.locafy.locafy.controllers;
 
+import org.springframework.ui.Model;
+import com.locafy.locafy.domain.BusinessOwner;
+import com.locafy.locafy.repositories.BusinessOwnerRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BusinessOwnerController {
+
+    private final BusinessOwnerRepository businessOwnerRepository;
+
+    public BusinessOwnerController(BusinessOwnerRepository businessOwnerRepository) {
+        this.businessOwnerRepository = businessOwnerRepository;
+    }
 
     @GetMapping("/business-owner-home")
     public String businessOwnerHome() {
@@ -12,8 +26,33 @@ public class BusinessOwnerController {
     }
 
     @GetMapping("/business-owner-profile")
-    public String businessOwnerProfile() {
+    public String showBusinessOwnerProfile(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        BusinessOwner currentOwner = businessOwnerRepository.findByUsername(userDetails.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        model.addAttribute("owner", currentOwner);
         return "business-owner-profile";
+    }
+
+
+    @PostMapping("/business-owner-profile")
+    public String updateBusinessOwnerProfile(@ModelAttribute("owner") BusinessOwner formOwner,
+                                     @RequestParam(required = false) String password,
+                                     @RequestParam(required = false) String confirmPassword,
+                                     @AuthenticationPrincipal UserDetails userDetails) {
+        BusinessOwner currentOwner = businessOwnerRepository.findByUsername(userDetails.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        currentOwner.setPhoneNumber(formOwner.getPhoneNumber());
+        currentOwner.setAddress(formOwner.getAddress());
+        currentOwner.setPassword(formOwner.getPassword());
+
+        /*if (password != null && !password.isEmpty() && password.equals(confirmPassword)) {
+            currentLocal.setPassword(passwordEncoder.encode(password));
+        }*/
+
+        businessOwnerRepository.save(currentOwner);
+        return "redirect:/business-owner-profile";
     }
 
     @GetMapping("/business-owner")
